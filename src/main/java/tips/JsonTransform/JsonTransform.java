417f1c287e0103b554b9ff215b278bf78a8a0f5d
yckj2204 <zhouxinyao@cloudwalk.cn>
2019-12-19 09:55:44 +0800

    Json字符串——>HashMap

--- src/main/java/tips/JsonTransform/JsonTransform.java
+++ src/main/java/tips/JsonTransform/JsonTransform.java
@@ -1,9 +1,12 @@
 package tips.JsonTransform;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Json字符串转换
@@ -12,7 +15,7 @@ import java.util.HashMap;
 public class JsonTransform {
 
     /**
-     * Json字符串——>HashMap
+     * Json字符串——>HashMap 方式一
      */
     @Test
     public void testJsonToHashMap() {
@@ -21,6 +24,22 @@ public class JsonTransform {
         /**
          {name=xiaohong, age=18}
          */
+    }
+
+    /**
+     * Json字符串——>HashMap 方式二
+     */
+    @Test
+    public void testJsonToHashMapByTypeReference() {
+        String s1 = "{\n" +
+                "\t\"registerNum\":\t\"32058\",\n" +
+                "\t\"businessType\":\t\"有限责任公司(自然人独资)\",\n" +
+                "}";
+//        Map<String, Object> object = JSONObject.parseObject(s1);
+
+        Map<String, String> userMap = JSON.parseObject(s1, new TypeReference<HashMap<String, String>>(){});
+
+        System.out.println(userMap);
     }
 
 }
