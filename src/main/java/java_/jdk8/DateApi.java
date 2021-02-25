8d2d94d995b6359645bf3c5b4c0eeabbc3358805
yaoyao <905440208@qq.com>
2021-02-25 17:00:33 +0800

    判断当前时间是否在[start, end]区间

--- src/main/java/java_/jdk8/DateApi.java
+++ src/main/java/java_/jdk8/DateApi.java
@@ -1,11 +1,10 @@
 package java_.jdk8;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.text.SimpleDateFormat;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -95,12 +94,68 @@ public class DateApi {
         return LocalDateTime.ofInstant(Instant.ofEpochMilli(mill), ZoneOffset.ofHours(8));
     }
 
+    /**
+     * 获取 年月日 时分秒
+     */
+    @Test
+    public void queryDate() {
+        Long timeStamp = 1614238651000L;
+        LocalDateTime localDateTime = millToDate(timeStamp);
+        int year = localDateTime.getYear();
+        int monthValue = localDateTime.getMonthValue();
+        int dayOfMonth = localDateTime.getDayOfMonth();
+        DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
+        int hour = localDateTime.getHour();
+        int minute = localDateTime.getMinute();
+        int second = localDateTime.getSecond();
+        System.out.println("year:" + year);
+        System.out.println("monthValue:" + monthValue);
+        System.out.println("dayOfMonth:" + dayOfMonth);
+        System.out.println("dayOfWeek:" + dayOfWeek.getValue());
+        System.out.println("hour:" + hour);
+        System.out.println("minute:" + minute);
+        System.out.println("second:" + second);
+    }
+
+    /**
+     * 判断当前时间是否在[start, end]区间
+     *
+     * @param now    当前时间
+     * @param start  开始时间
+     * @param end    结束时间
+     * @param format 时间格式
+     * @return
+     */
+    public static boolean timeIsInRound(String now, String start, String end, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        Date nowTime = null;
+        Date beginTime = null;
+        Date endTime = null;
+
+        try {
+            nowTime = df.parse(now);
+            beginTime = df.parse(start);
+            endTime = df.parse(end);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (nowTime.getTime() >= beginTime.getTime() && nowTime.getTime() <= endTime.getTime()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 //        List<Integer> dayList = getBetweenDays(1607043633000L, 1612400433000L);
         List<Integer> dayList = getBetweenDays(1612248752000L, 1612454400000L);
         List<Integer> monthList = getBetweenMonths(1607043633000L, 1612400433000L);
         log.info("dayList: [{}]", dayList);
         log.info("monthList: [{}]", monthList);
+
+        //比较一个 HH:mm:ss 是否在一个时间段内
+        System.out.println(timeIsInRound("15:10:02", "13:00:00", "21:30:00", "HH:mm:ss"));
     }
 
 }
